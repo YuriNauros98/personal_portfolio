7a37aeef00996277e848d724afb61cd35da52942
Yuri <navros.yuri@gmail.com>
2022-11-27 13:18:47 +0000

    Remove old stuff

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-^d4w@dv-9_vqt3%yd^pz(%xiodj4-)%0^o(h!9dd0cc2!0mt@%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['navyuri.pythonanywhere.com']
 
 
 # Application definition
@@ -112,6 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
